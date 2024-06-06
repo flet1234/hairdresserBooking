fd6044684f6516f6ae0e308d8347c03545fe11fa
flet1234 <flet1908@gmail.com>
2024-06-06 15:30:37 +0300

    fixshmix

--- frontEnd/src/features/TimeChooser.tsx
+++ frontEnd/src/features/TimeChooser.tsx
@@ -1,17 +1,17 @@
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-const TimeChooser = () => {
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+// const TimeChooser = () => {
 
-    return(
-        <>
-            <h1>time</h1>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateCalendar 
-                showDaysOutsideCurrentMonth
-                fixedWeekNumber={6}
-                />
-            </LocalizationProvider>
-        </>
-    )
-}
+//     return(
+//         <>
+//             <h1>time</h1>
+//             <LocalizationProvider dateAdapter={AdapterDayjs}>
+//                 <DateCalendar 
+//                 showDaysOutsideCurrentMonth
+//                 fixedWeekNumber={6}
+//                 />
+//             </LocalizationProvider>
+//         </>
+//     )
+// }
