6a033ef0188f51232205ee328520dcf39d2c17b9
flet1234 <flet1908@gmail.com>
2024-06-06 15:28:55 +0300

    bugfix

--- frontEnd/src/auth/CheckAdmin.tsx
+++ frontEnd/src/auth/CheckAdmin.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import axios from "axios";
 import {useAuthContext} from '../App'
-import LoginRegister from "../features/LoginRegister";
+
 import {ProviderProps} from "../../types/consts";
 
 const CheckAdmin = ({children}: ProviderProps) => {
-    const [redirect,setRedirect]=useState(false)
     const {setAdmin} = useAuthContext()
     const email = localStorage.getItem('email')
 
@@ -18,12 +17,10 @@ const CheckAdmin = ({children}: ProviderProps) => {
         try {
             const response = await axios.post(import.meta.env.VITE_API_URL+'/api/users/verify/admin',{email})
             if(response.status === 200){
-                setRedirect(true)
                 setAdmin(true)
             } 
         } catch (error) {
             console.log('Not logged in');
-            setRedirect(false)
         }
     }
     return children
